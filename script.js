ec14a600b4857677607f1fb3e7547b84689f177e
92castro <celin92castro@gmail.com>
2022-10-29 11:57:46 -0600

    started working on JS and wrote down steps

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+//declaring character list
+var UpperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+var specialChar = ["!", "@", "#", "$", "%", "&", "*"];
+
+//User length input
+var passwordLength;
+function generatePassword() {
+  var generate;
+  passwordLength = prompt(
+    "PLease choose how long you would like your password to be. Must be between 8 - 128 characters"
+  );
+  if (passwordLength > 7 && passwordLength < 129) {
+    passwordLength = prompt("Would you like Upper Case letters?");
+  } else {
+    passwordLength = prompt("Please try again.");
+  }
+}
+
+//makes Data type number instead of string
+
+//confirms for characters
+
+//check booleans confirms
+
+//final password
+
+//Get references to the #generate
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,7 +94,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
