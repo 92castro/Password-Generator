ae9cb060d06cb6d1498f533a34fa8c263fdf24e7
92castro <celin92castro@gmail.com>
2022-10-30 19:59:28 -0600

    submiting final code for function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //declaring character list
-var UpperCase = [
+var upperCaseArr = [
   "A",
   "B",
   "C",
@@ -27,8 +27,7 @@ var UpperCase = [
   "Y",
   "Z",
 ];
-
-var lowerCase = [
+var lowerCaseArr = [
   "a",
   "b",
   "c",
@@ -56,32 +55,55 @@ var lowerCase = [
   "y",
   "z",
 ];
-
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
-var specialChar = ["!", "@", "#", "$", "%", "&", "*"];
+var numbersArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharArr = ["!", "@", "#", "$", "%", "&", "*", "?"];
 
 //User length input
 var passwordLength;
 function generatePassword() {
-  var generate;
   passwordLength = prompt(
     "PLease choose how long you would like your password to be. Must be between 8 - 128 characters"
   );
-  if (passwordLength > 7 && passwordLength < 129) {
-    passwordLength = prompt("Would you like Upper Case letters?");
+  if (passwordLength >= 8 && passwordLength <= 128) {
   } else {
-    passwordLength = prompt("Please try again.");
+    passwordLength = alert("Please click 'Generate Password' and try again");
+    return;
   }
+  //run this when if statement is true
+  var upperCase = confirm("Would you like to include uppercase letters?");
+  var lowerCase = confirm("Would you like to include lowercase letters?");
+  var numbers = confirm("Would you like to include numbers?");
+  var specialChar = confirm("Would you like to include special characters?");
+  //confirming character selection
+  if (upperCase || lowerCase || numbers || specialChar) {
+    alert("Generating your new password...");
+  } else {
+    alert("You must choose one. Please try again.");
+  }
+
+  var passwordArr = [];
+  //check booleans of confirms
+  if (upperCase === true) {
+    passwordArr = passwordArr.concat(upperCaseArr);
+  }
+  if (lowerCase === true) {
+    passwordArr = passwordArr.concat(lowerCaseArr);
+  }
+  if (numbers === true) {
+    passwordArr = passwordArr.concat(numbersArr);
+  }
+  if (specialChar === true) {
+    passwordArr = passwordArr.concat(specialCharArr);
+  }
+  console.log(passwordArr);
+  //generate random password with loop
+  var newPassword = [];
+  for (var i = 0; i < passwordLength; i++) {
+    var random = Math.floor(Math.random() * passwordArr.length);
+    newPassword = newPassword + passwordArr[random];
+  }
+  return newPassword;
 }
-
-//makes Data type number instead of string
-
-//confirms for characters
-
-//check booleans confirms
-
-//final password
 
 //Get references to the #generate
 
